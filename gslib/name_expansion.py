fd41a819193a953ee780fa4ea90c9389937d967e
Toby Burress <kurin@delete.org>
2013-03-11 22:56:36 -0400

    NameExpansionResults handle has_plurality better.

--- gslib/name_expansion.py
+++ gslib/name_expansion.py
@@ -203,6 +203,7 @@ class _NameExpansionIterator(object):
     self.have_existing_dst_container = have_existing_dst_container
     self.flat = flat
     self.all_versions = all_versions
+    self.ever_had_plurality = self.uri_strs.has_plurality()
 
     # Map holding wildcard strings to use for flat vs subdir-by-subdir listings.
     # (A flat listing means show all objects expanded all the way down.)
@@ -243,7 +244,7 @@ class _NameExpansionIterator(object):
       wc = self._flatness_wildcard[self.flat]
       src_uri_expands_to_multi = (post_step1_iter.has_plurality()
                                   or post_step2_iter.has_plurality())
-      is_multi_src_request = (self.uri_strs.has_plurality()
+      is_multi_src_request = (self.ever_had_plurality
                               or src_uri_expands_to_multi)
 
       if post_step2_iter.is_empty():
