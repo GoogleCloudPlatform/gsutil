GoogleCloudPlatform/gsutil

commit 1acd9dc49a1544b7e68ba4f133f34abc629006f4

Fix name expansion bug for case where uri_strs is itself an iterator (issue #131)

gslib/name_expansion.py
@@ -203,6 +203,9 @@ class _NameExpansionIterator(object):
     self.have_existing_dst_container = have_existing_dst_container
     self.flat = flat
     self.all_versions = all_versions
+    # Check self.uri_strs.has_plurality() at start because its value can change
+    # if uri_strs is itself an iterator.
+    self.uri_strs.has_plurality = self.uri_strs.has_plurality()
 
     # Map holding wildcard strings to use for flat vs subdir-by-subdir listings.
     # (A flat listing means show all objects expanded all the way down.)
@@ -243,7 +246,7 @@ class _NameExpansionIterator(object):
       wc = self._flatness_wildcard[self.flat]
       src_uri_expands_to_multi = (post_step1_iter.has_plurality()
                                   or post_step2_iter.has_plurality())
-      is_multi_src_request = (self.uri_strs.has_plurality()
+      is_multi_src_request = (self.uri_strs.has_plurality
                               or src_uri_expands_to_multi)
 
       if post_step2_iter.is_empty():

gslib/commands/cp.py
@@ -1565,7 +1565,7 @@ class CpCommand(Command):
     else:
       if len(self.args) < 2:
         raise CommandException('Wrong number of arguments for "cp" command.')
-      uri_strs = self.args[0:len(self.args)-1]
+      uri_strs = self.args[:-1]
 
     (exp_dst_uri, have_existing_dst_container) = self._ExpandDstUri(
          self.args[-1])

commit 0fbf7c9fdaaae8a773146e75d2cefb0339ea50fb

Fixed bug introduced by 1acd9dc49a1544b7e68ba4f133f34abc629006f4, implemented additional naming unit test

gslib/name_expansion.py
@@ -278,7 +278,7 @@ class _NameExpansionIterator(object):
             self._WildcardIterator(uri_to_iterate))
         src_uri_expands_to_multi = (src_uri_expands_to_multi
                                     or wc_iter.has_plurality())
-        is_multi_src_request = (self.uri_strs.has_plurality()
+        is_multi_src_request = (self.uri_strs.has_plurality
                                 or src_uri_expands_to_multi)
         for blr in wc_iter:
           yield NameExpansionResult(uri_str, is_multi_src_request,

gslib/tests/test_naming.py
@@ -73,6 +73,18 @@ class GsutilNamingTests(testcase.GsUtilUnitTestCase):
     self.assertEqual(1, len(actual))
     self.assertEqual('f0', actual[0].object_name)
 
+  def testCopyingMultipleFilesToBucket(self):
+    """Tests copying multiple files to a bucket"""
+    src_file0 = self.CreateTempFile(file_name='f0')
+    src_file1 = self.CreateTempFile(file_name='f1')
+    dst_bucket_uri = self.CreateBucket()
+    self.RunCommand('cp', [src_file0, src_file1, suri(dst_bucket_uri)])
+    actual = list(self._test_wildcard_iterator(
+        suri(dst_bucket_uri, '**')).IterUris())
+    self.assertEqual(2, len(actual))
+    self.assertEqual('f0', actual[0].object_name)
+    self.assertEqual('f1', actual[1].object_name)
+
   def testCopyingAbsolutePathDirToBucket(self):
     """Tests recursively copying absolute path directory to a bucket"""
     dst_bucket_uri = self.CreateBucket()