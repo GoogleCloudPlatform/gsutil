GoogleCloudPlatform/gsutil

commit f1b99ea0ceffda7ce744d8d833dd2f86ff6a00b9

Make rsync -C work as documented.

gslib/copy_helper.py
@@ -1660,7 +1660,6 @@ def _UploadFileToObject(src_url, src_obj_filestream, src_obj_size,
             'temporary file is still locked.', upload_url.object_name)
     # In the gzip case, this is the gzip stream.  _CompressFileForUpload will
     # have already closed the original source stream.
-    upload_stream.close()
 
   if not parallel_composite_upload:
     try:
@@ -2742,8 +2741,14 @@ def PerformCopy(logger, src_url, dst_url, gsutil_api, command_obj,
     try:
       src_obj_filestream = GetStreamFromFileUrl(src_url)
     except Exception, e:  # pylint: disable=broad-except
-      raise CommandException('Error opening file "%s": %s.' % (src_url,
-                                                               e.message))
+      if command_obj.continue_on_error:
+        message = 'Error copying %s: %s' % (src_url, str(e))
+        command_obj.op_failure_count += 1
+        logger.error(message)
+        return
+      else:
+        raise CommandException('Error opening file "%s": %s.' % (src_url,
+                                                                 e.message))
     if src_url.IsStream():
       src_obj_size = None
     else:

gslib/tests/test_rsync.py
@@ -1042,3 +1042,30 @@ class TestRsync(testcase.GsUtilIntegrationTestCase):
           ['rsync', '-d', '-x', 'obj[34]', tmpdir, suri(bucket_uri)],
           return_stderr=True))
     _Check2()
+
+  @unittest.skipIf(IS_WINDOWS,
+                   "os.chmod() won't make file unreadable on Windows.")
+  def test_dir_to_bucket_minus_C(self):
+    """Tests that rsync -C option works correctly."""
+    # Create dir with 3 objects, the middle of which is unreadable.
+    tmpdir = self.CreateTempDir()
+    bucket_uri = self.CreateBucket()
+    self.CreateTempFile(tmpdir=tmpdir, file_name='obj1', contents='obj1')
+    path = self.CreateTempFile(tmpdir=tmpdir, file_name='obj2', contents='obj2')
+    os.chmod(path, 0)
+    self.CreateTempFile(tmpdir=tmpdir, file_name='obj3', contents='obj3')
+
+    # Use @Retry as hedge against bucket listing eventual consistency.
+    @Retry(AssertionError, tries=3, timeout_secs=1)
+    def _Check():
+      """Tests rsync works as expected."""
+      stderr =  self.RunGsUtil(['rsync', '-C', tmpdir, suri(bucket_uri)],
+                               expected_status=1, return_stderr=True)
+      self.assertIn('1 files/objects could not be copied/removed.', stderr)
+      listing1 = _TailSet(tmpdir, self._FlatListDir(tmpdir))
+      listing2 = _TailSet(suri(bucket_uri), self._FlatListBucket(bucket_uri))
+      # Dir should have un-altered content.
+      self.assertEquals(listing1, set(['/obj1', '/obj2', '/obj3']))
+      # Bucket should have obj1 and obj3 even though obj2 was unreadable.
+      self.assertEquals(listing2, set(['/obj1', '/obj3']))
+    _Check()

commit b8f17e0a1ae3992f4cf8b064820ea57eae3cae55

Fix bug introduced in f1b99ea0ceffda7ce744d8d833dd2f86ff6a00b9

gslib/copy_helper.py
@@ -1660,6 +1660,7 @@ def _UploadFileToObject(src_url, src_obj_filestream, src_obj_size,
             'temporary file is still locked.', upload_url.object_name)
     # In the gzip case, this is the gzip stream.  _CompressFileForUpload will
     # have already closed the original source stream.
+    upload_stream.close()
 
   if not parallel_composite_upload:
     try: