754f3268a77d401fb630e7f4762ef5aa32e636de
Travis Hobrla <thobrla@google.com>
2016-07-25 12:04:24 -0700

    Fix path bug for test_repo_matches_manifest + Travis

--- gslib/tests/test_update.py
+++ gslib/tests/test_update.py
@@ -186,7 +186,8 @@ class UpdateUnitTest(testcase.GsUtilUnitTestCase):
 
     manifest_lines = ['gslib', 'third_party', 'MANIFEST.in']
 
-    with open(os.path.join(GSUTIL_DIR, 'MANIFEST.in'), 'r') as fp:
+    with open(os.path.join(os.path.realpath(sys.argv[0]), 'MANIFEST.in'),
+              'r') as fp:
       for line in fp:
         if line.startswith('include '):
           manifest_lines.append(line.split()[-1])
