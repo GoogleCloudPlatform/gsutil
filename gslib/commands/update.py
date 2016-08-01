2cd0652915446b4a19c569c0ec949f0c8ad90c7e
Joshua Kahn <j.harris.kahn@gmail.com>
2016-08-01 13:04:51 -0700

    Fix failing builds in Travis CI.

--- gslib/commands/update.py
+++ gslib/commands/update.py
@@ -16,9 +16,7 @@
 
 from __future__ import absolute_import
 
-import logging
 import os
-import re
 import shutil
 import signal
 import stat
@@ -34,6 +32,7 @@ from gslib.exception import CommandException
 from gslib.sig_handling import RegisterSignalHandler
 from gslib.util import CERTIFICATE_VALIDATION_ENABLED
 from gslib.util import CompareVersions
+from gslib.util import DisallowUpdateIfDataInGsutilDir
 from gslib.util import GetBotoConfigFileList
 from gslib.util import GSUTIL_PUB_TARBALL
 from gslib.util import IS_CYGWIN
@@ -91,59 +90,6 @@ _DETAILED_HELP_TEXT = ("""
   -n          Causes update command to run without prompting [Y/n] whether to
               continue if an update is available.
 """ % GSUTIL_PUB_TARBALL)
-
-
-def DisallowUpdataIfDataInGsutilDir(directory=gslib.GSUTIL_DIR):
-  """Disallows the update command if files not in the gsutil distro are found.
-
-  This prevents users from losing data if they are in the habit of running
-  gsutil from the gsutil directory and leaving data in that directory.
-
-  This will also detect someone attempting to run gsutil update from a git
-  repo, since the top-level directory will contain git files and dirs (like
-  .git) that are not distributed with gsutil.
-
-  Args:
-    directory: The directory to use this functionality on.
-
-  Raises:
-    CommandException: if files other than those distributed with gsutil found.
-  """
-  # Manifest includes recursive-includes of gslib. Directly add
-  # those to the list here so we will skip them in os.listdir() loop without
-  # having to build deeper handling of the MANIFEST file here. Also include
-  # 'third_party', which isn't present in manifest but gets added to the
-  # gsutil distro by the gsutil submodule configuration; and the MANIFEST.in
-  # and CHANGES.md files.
-  manifest_lines = ['MANIFEST.in', 'third_party']
-
-  try:
-    with open(os.path.join(directory, 'MANIFEST.in'), 'r') as fp:
-      for line in fp:
-        if line.startswith('include '):
-          manifest_lines.append(line.split()[-1])
-        elif re.match(r'recursive-include \w+ \*', line):
-          manifest_lines.append(line.split()[1])
-  except IOError:
-    logging.getLogger().warn('MANIFEST.in not found in %s.\nSkipping user data '
-                             'check.\n', directory)
-    return
-
-  # Look just at top-level directory. We don't try to catch data dropped into
-  # subdirs (like gslib) because that would require deeper parsing of
-  # MANFFEST.in, and most users who drop data into gsutil dir do so at the top
-  # level directory.
-  for filename in os.listdir(directory):
-    if (filename.endswith('.pyc') or filename == '__pycache__'
-        or filename == '.travis.yml'):
-      # Ignore compiled code and travis config.
-      continue
-    if filename not in manifest_lines:
-      raise CommandException('\n'.join(textwrap.wrap(
-          'A file (%s) that is not distributed with gsutil was found in '
-          'the gsutil directory. The update command cannot run with user '
-          'data in the gsutil directory.' %
-          os.path.join(gslib.GSUTIL_DIR, filename))))
 
 
 class UpdateCommand(Command):
@@ -295,7 +241,7 @@ class UpdateCommand(Command):
           'Your boto configuration has https_validate_certificates = False.\n'
           'The update command cannot be run this way, for security reasons.')
 
-    DisallowUpdataIfDataInGsutilDir()
+    DisallowUpdateIfDataInGsutilDir()
 
     force_update = False
     no_prompt = False
--- gslib/util.py
+++ gslib/util.py
@@ -198,6 +198,62 @@ cached_multiprocessing_is_available_stack_trace = None
 cached_multiprocessing_is_available_message = None
 
 
+# This function used to belong inside of update.py. However, it needed to be
+# moved here due to compatibility issues with Travis CI, because update.py is
+# not included with PyPI installations.
+def DisallowUpdateIfDataInGsutilDir(directory=gslib.GSUTIL_DIR):
+  """Disallows the update command if files not in the gsutil distro are found.
+
+  This prevents users from losing data if they are in the habit of running
+  gsutil from the gsutil directory and leaving data in that directory.
+
+  This will also detect someone attempting to run gsutil update from a git
+  repo, since the top-level directory will contain git files and dirs (like
+  .git) that are not distributed with gsutil.
+
+  Args:
+    directory: The directory to use this functionality on.
+
+  Raises:
+    CommandException: if files other than those distributed with gsutil found.
+  """
+  # Manifest includes recursive-includes of gslib. Directly add
+  # those to the list here so we will skip them in os.listdir() loop without
+  # having to build deeper handling of the MANIFEST file here. Also include
+  # 'third_party', which isn't present in manifest but gets added to the
+  # gsutil distro by the gsutil submodule configuration; and the MANIFEST.in
+  # and CHANGES.md files.
+  manifest_lines = ['MANIFEST.in', 'third_party']
+
+  try:
+    with open(os.path.join(directory, 'MANIFEST.in'), 'r') as fp:
+      for line in fp:
+        if line.startswith('include '):
+          manifest_lines.append(line.split()[-1])
+        elif re.match(r'recursive-include \w+ \*', line):
+          manifest_lines.append(line.split()[1])
+  except IOError:
+    logging.getLogger().warn('MANIFEST.in not found in %s.\nSkipping user data '
+                             'check.\n', directory)
+    return
+
+  # Look just at top-level directory. We don't try to catch data dropped into
+  # subdirs (like gslib) because that would require deeper parsing of
+  # MANFFEST.in, and most users who drop data into gsutil dir do so at the top
+  # level directory.
+  for filename in os.listdir(directory):
+    if (filename.endswith('.pyc') or filename == '__pycache__'
+        or filename == '.travis.yml'):
+      # Ignore compiled code and travis config.
+      continue
+    if filename not in manifest_lines:
+      raise CommandException('\n'.join(textwrap.wrap(
+          'A file (%s) that is not distributed with gsutil was found in '
+          'the gsutil directory. The update command cannot run with user '
+          'data in the gsutil directory.' %
+          os.path.join(gslib.GSUTIL_DIR, filename))))
+
+
 # This class is necessary to convert timestamps to UTC. By default Python
 # datetime objects are timezone unaware. This created problems when interacting
 # with cloud object timestamps which are timezone aware. This issue appeared
