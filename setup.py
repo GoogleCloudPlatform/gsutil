884773584f868edf068a0bdeadf799536824ffb0
Matt Houglum <houglum@google.com>
2018-01-16 15:06:10 -0800

    Update to apitools v0.5.21.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ requires = [
     'crcmod>=1.7',
     'fasteners>=0.14.1',
     'gcs-oauth2-boto-plugin>=1.14',
-    'google-apitools==0.5.20',
+    'google-apitools==0.5.21',
     'httplib2>=0.10.3',
     # TODO: Sync submodule with tag referenced here once #339 is fixed in mock.
     'mock==2.0.0',
