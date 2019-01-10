81299d43eea4e6dbf68306656b3d9fa3929f7ff3
Matt Houglum <houglum@google.com>
2019-01-09 17:15:46 -0800

    Add crc32c instructions for enterprise SUSE

--- gslib/addlhelp/crc32c.py
+++ gslib/addlhelp/crc32c.py
@@ -98,6 +98,15 @@ _DETAILED_HELP_TEXT = ("""
     sudo pip uninstall crcmod
     sudo pip install -U crcmod
 
+  Enterprise SUSE
+  -----------------
+
+  To compile and install crcmod:
+
+    sudo zypper install gcc python-devel
+    sudo pip uninstall crcmod
+    sudo pip install -U crcmod
+
   macOS
   -----
 
