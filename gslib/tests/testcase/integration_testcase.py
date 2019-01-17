186b0f0963a549e1468129f6c34ffbb918ac878b
Cat Lee Ball 🎷🐛 <cball@google.com>
2019-01-17 15:26:21 -0800

    Added `seed` argument to get_random_ascii_chars
    Per feedback on PR, adding a seed argument to this method to
    allow future authors to specify how random or deterministic
    they would like their characters to be.

--- gslib/tests/testcase/integration_testcase.py
+++ gslib/tests/testcase/integration_testcase.py
@@ -276,16 +276,18 @@ class GsUtilIntegrationTestCase(base.GsUtilTestCase):
       self.xml_api.PatchObjectMetadata(bucket_name, object_name, obj_metadata,
                                        provider=provider)
 
-  def get_random_ascii_chars(self, size):
+  def get_random_ascii_chars(self, size, seed=None):
     """Generates random ASCII characters up to a given size.
 
     Args:
       size: Integer quantity of characters to generate.
+      seed: A seed may be specified for deterministic behavior.
+            None is used as the default value.
 
     Returns:
       String of length equal to size argument.
     """
-    random.seed(0)
+    random.seed(seed)
     charset = string.printable.encode('ascii')
 
     contents = ''.join([random.choice(charset) for _ in range(size)]
