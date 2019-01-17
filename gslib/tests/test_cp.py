bfab63f6fc69c79032ace5349c8d33bbd9b299ce
Kevin Locke <kevin@kevinlocke.name>
2019-01-17 19:22:55 +0000

    Don't clobber local files with differing size (#605)
    As discussed in #591, `gsutil cp -n` will overwrite an existing local
    file if its size differs from the size of the source file.  This
    behavior is contrary to user expectations, POSIX convention, and the
    documentation.
    
    Remove the special case for files with differing size, which was added
    in a768a58 to support resuming partial downloads.  This code has not
    been useful since 3e3d6dc, which started using temporary files (named
    with _.gstmp) for all downloads.
    
    Fixes: #591
    
    Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

--- gslib/tests/test_cp.py
+++ gslib/tests/test_cp.py
@@ -466,6 +466,20 @@ class TestCp(testcase.GsUtilIntegrationTestCase):
       self.assertIn('Skipping existing item: %s' % suri(f), stderr)
       self.assertEqual(f.read(), 'bar')
 
+  @SequentialAndParallelTransfer
+  def test_noclobber_different_size(self):
+    key_uri = self.CreateObject(contents='foo')
+    fpath = self.CreateTempFile(contents='quux')
+    stderr = self.RunGsUtil(['cp', '-n', fpath, suri(key_uri)],
+                            return_stderr=True)
+    self.assertIn('Skipping existing item: %s' % suri(key_uri), stderr)
+    self.assertEqual(key_uri.get_contents_as_string(), 'foo')
+    stderr = self.RunGsUtil(['cp', '-n', suri(key_uri), fpath],
+                            return_stderr=True)
+    with open(fpath, 'r') as f:
+      self.assertIn('Skipping existing item: %s' % suri(f), stderr)
+      self.assertEqual(f.read(), 'quux')
+
   def test_dest_bucket_not_exist(self):
     fpath = self.CreateTempFile(contents='foo')
     invalid_bucket_uri = (
