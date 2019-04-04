2ea8ea4546f92239a1c5586c070521ab8c8d5c1f
Joe Walker <walkerjoseph@google.com>
2019-04-04 13:08:11 -0700

    Code comments added for clarification

--- gslib/utils/system_util.py
+++ gslib/utils/system_util.py
@@ -250,7 +250,9 @@ def IsRunningInteractively():
 
 
 def MonkeyPatchHttp():
+  # Getting python version array in the following format: [major, minor, micro]
   py_ver = [int(part) for part in platform.python_version().split('.')]
+  # Checking for and applying monkeypatch to python versions 3.4 - 3.6.7
   if (py_ver[0] == 3 and
       4 <= py_ver[1] < 6 or (py_ver[1] == 6 and py_ver[2] < 8)):
     _MonkeyPatchHttpForPython_3x()
