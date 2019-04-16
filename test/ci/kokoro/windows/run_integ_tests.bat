fd056c55ecba34df988f27696b2e3b35d576427a
Lee Ball 🎷🐛 <cball@google.com>
2019-04-15 23:43:43 -0700

    Added license text

--- test/ci/kokoro/windows/run_integ_tests.bat
+++ test/ci/kokoro/windows/run_integ_tests.bat
@@ -15,9 +15,6 @@ rem limitations under the License.
 rem Kokoro looks for a .bat build file, but all our logic is actually in
 rem a PowerShell script. This simply launches our script with the appropriate
 rem parameters.
-rem
-rem https://stackoverflow.com/questions/19335004/how-to-run-a-powershell-script-from-a-batch-file
-rem http://blog.danskingdom.com/allow-others-to-run-your-powershell-scripts-from-a-batch-file-they-will-love-you-for-it/
 
 set GsutilRepoDir="T:\src\github\src\gsutil"
 set "PyExePath=C:\python%PYMAJOR%%PYMINOR%\python.exe"
