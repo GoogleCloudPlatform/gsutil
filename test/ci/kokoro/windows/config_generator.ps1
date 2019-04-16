946cf48fca24ea9a2f9f199c660aecd786f8e3ce
Lee Ball 🎷🐛 <cball@google.com>
2019-04-15 21:03:05 -0700

    Rewrote config generator in Powershell

---
+++ test/ci/kokoro/windows/config_generator.ps1
@@ -0,0 +1,20 @@
+param (
+    [Parameter(Mandatory=$true)][string]$keyfile,
+    [Parameter(Mandatory=$true)][string]$api,
+    [Parameter(Mandatory=$true)][string]$outfile
+ )
+
+$key = [IO.File]::ReadAllText($keyfile)
+
+$stream = [System.IO.StreamWriter] $outfile
+$stream.WriteLine("[Credentials]")
+$stream.WriteLine("gs_service_key_file = $key")
+$stream.WriteLine("[GSUtil]")
+$stream.WriteLine("test_notification_url = https://bigstore-test-notify.appspot.com/notify")
+$stream.WriteLine("default_project_id = bigstore-gsutil-testing")
+$stream.WriteLine("prefer_api = $api")
+$stream.WriteLine("[OAuth2]")
+$stream.WriteLine("client_id = 909320924072.apps.googleusercontent.com")
+$stream.WriteLine("client_secret = p3RlpR10xMFh9ZXBS/ZNLYUu")
+$stream.close()
+
