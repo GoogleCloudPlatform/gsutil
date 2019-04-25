a62a62b199f0fb22abcd1c5f0a7e1c3a3e445ef6
Cat Lee Ball 🎷🐛 <cball@google.com>
2019-04-25 11:19:29 -0700

    Using --target

--- test/ci/kokoro/windows/run_integ_tests.bat
+++ test/ci/kokoro/windows/run_integ_tests.bat
@@ -25,7 +25,7 @@ type %BOTO_CONFIG%
 
 cd %GsutilRepoDir%
 git submodule update --init --recursive
-%PyExePath% -m %PipPath% install -U crcmod
+%PipPath% install crcmod --target "C:\python%PYMAJOR%%PYMINOR%\files\Lib\site-packages"
 
 rem Print config info prior to running tests
 %PyExePath% %GsutilRepoDir%\gsutil.py version -l
