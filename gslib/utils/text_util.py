78daa2ee1b1f713755d67ffea19a93478fbb5c2b
Lee Ball <43632885+catleeball@users.noreply.github.com>
2019-05-06 22:53:45 -0700

    Remove trailing '/' strip
    Not necessary now that we have '/' in the allowed symbols filter.

--- gslib/utils/text_util.py
+++ gslib/utils/text_util.py
@@ -327,9 +327,6 @@ def IsBucketNameValid(bucket_name):
   prefix, url = bucket_name.split('://')
   url = url.rstrip()
 
-  if url[-1] == '/':
-    url = url[:-1]
-
   return all([
     prefix.isalpha(),
     prefix.islower(),
