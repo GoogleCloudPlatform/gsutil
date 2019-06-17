13fd6ceadc50759532037f6a7e7a5b847a07a150
Matt Houglum <houglum@google.com>
2019-06-17 15:40:57 -0700

    Test `help` command for all commands. (#796)
    This will also be run against new commands added in the future, since
    the list of commands is obtained by a call to Command.__subclasses__().

--- gslib/tests/test_help.py
+++ gslib/tests/test_help.py
@@ -26,6 +26,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
+from gslib.command import Command
 import gslib.tests.testcase as testcase
 
 
@@ -73,3 +74,11 @@ class HelpIntegrationTests(testcase.GsUtilIntegrationTestCase):
   def test_help_wrong_num_args(self):
     stderr = self.RunGsUtil(['cp'], return_stderr=True, expected_status=1)
     self.assertIn('Usage:', stderr)
+
+  def test_help_runs_for_all_commands(self):
+    # This test is particularly helpful because the `help` command can fail
+    # under unusual circumstances (e.g. someone adds a new command and they make
+    # the "one-line" summary longer than the defined character limit).
+    for command in Command.__subclasses__():
+      # Raises exception if the exit code is non-zero.
+      self.RunGsUtil(['help', command.command_spec.command_name])
