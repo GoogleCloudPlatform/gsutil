54113e8554ad7daee7774a562b0e6e9129c53689
Michelle Ross <michelleross@google.com>
2019-07-10 17:15:58 -0400

    add warning about release to hmac commands file

--- gslib/commands/hmac.py
+++ gslib/commands/hmac.py
@@ -12,7 +12,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Implementation of HMAC key management command for GCS."""
+"""
+Implementation of HMAC key management command for GCS.
+
+NOTE: Any modification to this file or corresponding HMAC logic
+should be submitted in its own PR and release
+"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
