GoogleCloudPlatform/gsutil

commit f86510bd886b7fc31e1a1fa3331db5e0133d71ef

Fix cp/mv; dest dir is a substring of another dir (#889)
Fix cp/mv; dest dir is a substring of another dir

Issue: #361
https://issuetracker.google.com/issues/112817360

gslib/utils/copy_helper.py
@@ -89,7 +89,6 @@ from gslib.progress_callback import ProgressCallbackWithTimeout
 from gslib.resumable_streaming_upload import ResumableStreamingJsonUploadWrapper
 from gslib.storage_url import ContainsWildcard
 from gslib.storage_url import GenerationFromUrlAndString
-from gslib.storage_url import IsCloudSubdirPlaceholder
 from gslib.storage_url import StorageUrlFromString
 from gslib.third_party.storage_apitools import storage_v1_messages as apitools_messages
 from gslib.thread_message import FileMessage
@@ -1435,21 +1434,20 @@ def ExpandUrlToSingleBlr(url_str,
   if storage_url.IsBucket():
     return (storage_url, True)
 
-  # For object/prefix URLs, there are four cases that indicate the destination
+  # For object/prefix URLs, there are three cases that indicate the destination
   # is a cloud subdirectory; these are always considered to be an existing
   # container. Checking each case allows gsutil to provide Unix-like
   # destination folder semantics, but requires up to three HTTP calls, noted
   # below.
 
-  # Case 1: If a placeholder object ending with '/' exists.
-  if IsCloudSubdirPlaceholder(storage_url):
-    return (storage_url, True)
+  # Get version of object name without trailing slash for matching prefixes
+  prefix = storage_url.object_name.rstrip('/')
 
   # HTTP call to make an eventually consistent check for a matching prefix,
   # _$folder$, or empty listing.
   expansion_empty = True
   list_iterator = gsutil_api.ListObjects(storage_url.bucket_name,
-                                         prefix=storage_url.object_name,
+                                         prefix=prefix,
                                          delimiter='/',
                                          provider=storage_url.scheme,
                                          fields=['prefixes', 'items/name'])
@@ -1462,23 +1460,24 @@ def ExpandUrlToSingleBlr(url_str,
     # If this case becomes common, we could heurestically abort the
     # listing operation after the first page of results and just query for the
     # _$folder$ object directly using GetObjectMetadata.
-    # TODO: curently the ListObjects iterator yields objects before prefixes,
+    # TODO: currently the ListObjects iterator yields objects before prefixes,
     # because ls depends on this iteration order for proper display.  We could
     # save up to 1ms in determining that a destination is a prefix if we had a
     # way to yield prefixes first, but this would require poking a major hole
     # through the abstraction to control this iteration order.
     expansion_empty = False
 
-    if obj_or_prefix.datatype == CloudApi.CsObjectOrPrefixType.PREFIX:
-      # Case 2: If there is a matching prefix when listing the destination URL.
+    if (obj_or_prefix.datatype == CloudApi.CsObjectOrPrefixType.PREFIX and
+        obj_or_prefix.data == prefix + '/'):
+      # Case 1: If there is a matching prefix when listing the destination URL.
       return (storage_url, True)
     elif (obj_or_prefix.datatype == CloudApi.CsObjectOrPrefixType.OBJECT and
           obj_or_prefix.data.name == storage_url.object_name + '_$folder$'):
-      # Case 3: If a placeholder object matching destination + _$folder$
+      # Case 2: If a placeholder object matching destination + _$folder$
       # exists.
       return (storage_url, True)
 
-  # Case 4: If no objects/prefixes matched, and nonexistent objects should be
+  # Case 3: If no objects/prefixes matched, and nonexistent objects should be
   # treated as subdirectories.
   return (storage_url, expansion_empty and treat_nonexistent_object_as_subdir)
 

gslib/tests/test_copy_helper_funcs.py
@@ -22,15 +22,18 @@ from __future__ import unicode_literals
 import datetime
 import logging
 import os
-
 from apitools.base.py import exceptions as apitools_exceptions
 
+from gslib.bucket_listing_ref import BucketListingObject
+from gslib.bucket_listing_ref import BucketListingPrefix
+from gslib.cloud_api import CloudApi
 from gslib.cloud_api import ResumableUploadAbortException
 from gslib.cloud_api import ResumableUploadException
 from gslib.cloud_api import ResumableUploadStartOverException
 from gslib.cloud_api import ServiceException
 from gslib.command import CreateOrGetGsutilLogger
 from gslib.discard_messages_queue import DiscardMessagesQueue
+from gslib.exception import CommandException
 from gslib.gcs_json_api import GcsJsonApi
 from gslib.parallel_tracker_file import ObjectFromTracker
 from gslib.storage_url import StorageUrlFromString
@@ -48,6 +51,7 @@ from gslib.utils.copy_helper import _DelegateUploadFileToObject
 from gslib.utils.copy_helper import _GetPartitionInfo
 from gslib.utils.copy_helper import _SelectUploadCompressionStrategy
 from gslib.utils.copy_helper import _SetContentTypeFromFile
+from gslib.utils.copy_helper import ExpandUrlToSingleBlr
 from gslib.utils.copy_helper import FilterExistingComponents
 from gslib.utils.copy_helper import GZIP_ALL_FILES
 from gslib.utils.copy_helper import PerformParallelUploadFileToObjectArgs
@@ -558,3 +562,158 @@ class TestCpFuncs(GsUtilUnitTestCase):
     self.assertTrue(mock_stream.close.called)
     # Ensure the lock was released.
     self.assertFalse(mock_lock.__exit__.called)
+
+
+class TestExpandUrlToSingleBlr(GsUtilUnitTestCase):
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.CreateWildcardIterator')
+  def testContainsWildcardMatchesNotObject(self, mock_CreateWildcardIterator,
+                                           mock_gsutil_api):
+    storage_url = StorageUrlFromString('gs://test/helloworld')
+    mock_CreateWildcardIterator.return_value = iter(
+        [BucketListingPrefix(storage_url)])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/hello*/', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertTrue(have_existing_dst_container)
+    self.assertEqual(exp_url, storage_url)
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.CreateWildcardIterator')
+  def testContainsWildcardMatchesObject(self, mock_CreateWildcardIterator,
+                                        mock_gsutil_api):
+    storage_url = StorageUrlFromString('gs://test/helloworld')
+    mock_CreateWildcardIterator.return_value = iter(
+        [BucketListingObject(storage_url)])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/hello*/', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertFalse(have_existing_dst_container)
+    self.assertEqual(exp_url, storage_url)
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.CreateWildcardIterator')
+  def testContainsWildcardMultipleMatches(self, mock_CreateWildcardIterator,
+                                          mock_gsutil_api):
+    mock_CreateWildcardIterator.return_value = iter([
+        BucketListingObject(StorageUrlFromString('gs://test/helloworld')),
+        BucketListingObject(StorageUrlFromString('gs://test/helloworld2'))
+    ])
+    with self.assertRaises(CommandException):
+      ExpandUrlToSingleBlr('gs://test/hello*/', mock_gsutil_api, 'project_id',
+                           False, CreateOrGetGsutilLogger('copy_test'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.CreateWildcardIterator')
+  def testContainsWildcardNoMatches(self, mock_CreateWildcardIterator,
+                                    mock_gsutil_api):
+    mock_CreateWildcardIterator.return_value = iter([])
+    with self.assertRaises(CommandException):
+      ExpandUrlToSingleBlr('gs://test/hello*/', mock_gsutil_api, 'project_id',
+                           False, CreateOrGetGsutilLogger('copy_test'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.StorageUrlFromString')
+  def testLocalFileDirectory(self, mock_StorageUrlFromString, mock_gsutil_api):
+    mock_storage_url = mock.Mock()
+    mock_storage_url.isFileUrl.return_value = True
+    mock_storage_url.IsDirectory.return_value = True
+    mock_StorageUrlFromString.return_value = mock_storage_url
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        '/home/test', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertTrue(have_existing_dst_container)
+    self.assertEqual(exp_url, mock_storage_url)
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  @mock.patch('gslib.utils.copy_helper.StorageUrlFromString')
+  def testLocalFileNotDirectory(self, mock_StorageUrlFromString,
+                                mock_gsutil_api):
+    mock_storage_url = mock.Mock()
+    mock_storage_url.isFileUrl.return_value = True
+    mock_storage_url.IsDirectory.return_value = False
+    mock_StorageUrlFromString.return_value = mock_storage_url
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        '/home/test', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertFalse(have_existing_dst_container)
+    self.assertEqual(exp_url, mock_storage_url)
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testNoSlashPrefixExactMatch(self, mock_gsutil_api):
+    mock_gsutil_api.ListObjects.return_value = iter([
+        CloudApi.CsObjectOrPrefix('folder/',
+                                  CloudApi.CsObjectOrPrefixType.PREFIX)
+    ])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertTrue(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testNoSlashPrefixSubstringMatch(self, mock_gsutil_api):
+    mock_gsutil_api.ListObjects.return_value = iter([
+        CloudApi.CsObjectOrPrefix('folderone/',
+                                  CloudApi.CsObjectOrPrefixType.PREFIX)
+    ])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertFalse(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testNoSlashFolderPlaceholder(self, mock_gsutil_api):
+    mock_gsutil_api.ListObjects.return_value = iter([
+        CloudApi.CsObjectOrPrefix(
+            apitools_messages.Object(name='folder_$folder$'),
+            CloudApi.CsObjectOrPrefixType.OBJECT)
+    ])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertTrue(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testNoSlashNoMatch(self, mock_gsutil_api):
+    mock_gsutil_api.ListObjects.return_value = iter([])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertFalse(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testWithSlashPrefixExactMatch(self, mock_gsutil_api):
+    mock_gsutil_api.ListObjects.return_value = iter([
+        CloudApi.CsObjectOrPrefix('folder/',
+                                  CloudApi.CsObjectOrPrefixType.PREFIX)
+    ])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder/', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertTrue(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder/'))
+
+  @mock.patch('gslib.cloud_api.CloudApi')
+  def testWithSlashNoMatch(self, mock_gsutil_api):
+    """Tests use case that was removed"""
+    mock_gsutil_api.ListObjects.return_value = iter([])
+    (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
+        'gs://test/folder/', mock_gsutil_api, 'project_id', False,
+        CreateOrGetGsutilLogger('copy_test'))
+
+    self.assertFalse(have_existing_dst_container)
+    self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder/'))

gslib/tests/test_naming.py
@@ -319,7 +319,7 @@ class GsutilNamingTests(testcase.GsUtilUnitTestCase):
     actual = set(
         str(u) for u in self._test_wildcard_iterator(suri(
             dst_bucket_uri, '**')).IterAll(expand_top_level_buckets=True))
-    expected = set([suri(dst_bucket_uri, 'abc', 'dir1', 'f1')])
+    expected = set([suri(dst_bucket_uri, 'abc', 'f1')])
     self.assertEqual(expected, actual)
 
   # @SequentialAndParallelTransfer

commit be3bdbb37014d1029b75b28f774a7e3a694251c3

Fix regression in cp caused by change #889.
The previously committed change caused a problem where running:

cp myFile gs://mybucket/nonExistentDir/

would create a file named "nonExistentDir" under "mybucket" instead of
creating "nonExistentDir/myFile".  Rather than try to special case this
I've just reverted part of the previous PR since expected behavior gets
gets ambiguous when providing multiple source files/directories.

gslib/utils/copy_helper.py
@@ -89,6 +89,7 @@ from gslib.progress_callback import ProgressCallbackWithTimeout
 from gslib.resumable_streaming_upload import ResumableStreamingJsonUploadWrapper
 from gslib.storage_url import ContainsWildcard
 from gslib.storage_url import GenerationFromUrlAndString
+from gslib.storage_url import IsCloudSubdirPlaceholder
 from gslib.storage_url import StorageUrlFromString
 from gslib.third_party.storage_apitools import storage_v1_messages as apitools_messages
 from gslib.thread_message import FileMessage
@@ -1434,11 +1435,15 @@ def ExpandUrlToSingleBlr(url_str,
   if storage_url.IsBucket():
     return (storage_url, True)
 
-  # For object/prefix URLs, there are three cases that indicate the destination
+  # For object/prefix URLs, there are four cases that indicate the destination
   # is a cloud subdirectory; these are always considered to be an existing
   # container. Checking each case allows gsutil to provide Unix-like
   # destination folder semantics, but requires up to three HTTP calls, noted
   # below.
+
+  # Case 1: If a placeholder object ending with '/' exists.
+  if IsCloudSubdirPlaceholder(storage_url):
+    return (storage_url, True)
 
   # Get version of object name without trailing slash for matching prefixes
   prefix = storage_url.object_name.rstrip('/')
@@ -1469,15 +1474,15 @@ def ExpandUrlToSingleBlr(url_str,
 
     if (obj_or_prefix.datatype == CloudApi.CsObjectOrPrefixType.PREFIX and
         obj_or_prefix.data == prefix + '/'):
-      # Case 1: If there is a matching prefix when listing the destination URL.
+      # Case 2: If there is a matching prefix when listing the destination URL.
       return (storage_url, True)
     elif (obj_or_prefix.datatype == CloudApi.CsObjectOrPrefixType.OBJECT and
           obj_or_prefix.data.name == storage_url.object_name + '_$folder$'):
-      # Case 2: If a placeholder object matching destination + _$folder$
+      # Case 3: If a placeholder object matching destination + _$folder$
       # exists.
       return (storage_url, True)
 
-  # Case 3: If no objects/prefixes matched, and nonexistent objects should be
+  # Case 4: If no objects/prefixes matched, and nonexistent objects should be
   # treated as subdirectories.
   return (storage_url, expansion_empty and treat_nonexistent_object_as_subdir)
 

gslib/tests/test_copy_helper_funcs.py
@@ -709,11 +709,10 @@ class TestExpandUrlToSingleBlr(GsUtilUnitTestCase):
 
   @mock.patch('gslib.cloud_api.CloudApi')
   def testWithSlashNoMatch(self, mock_gsutil_api):
-    """Tests use case that was removed"""
     mock_gsutil_api.ListObjects.return_value = iter([])
     (exp_url, have_existing_dst_container) = ExpandUrlToSingleBlr(
         'gs://test/folder/', mock_gsutil_api, 'project_id', False,
         CreateOrGetGsutilLogger('copy_test'))
 
-    self.assertFalse(have_existing_dst_container)
+    self.assertTrue(have_existing_dst_container)
     self.assertEqual(exp_url, StorageUrlFromString('gs://test/folder/'))

gslib/tests/test_naming.py
@@ -319,7 +319,7 @@ class GsutilNamingTests(testcase.GsUtilUnitTestCase):
     actual = set(
         str(u) for u in self._test_wildcard_iterator(suri(
             dst_bucket_uri, '**')).IterAll(expand_top_level_buckets=True))
-    expected = set([suri(dst_bucket_uri, 'abc', 'f1')])
+    expected = set([suri(dst_bucket_uri, 'abc', 'dir1', 'f1')])
     self.assertEqual(expected, actual)
 
   # @SequentialAndParallelTransfer
@@ -637,6 +637,20 @@ class GsutilNamingTests(testcase.GsUtilUnitTestCase):
     src_file = self.CreateTempFile(file_name='foo')
     dst_bucket_uri = self.CreateBucket()
     self.RunCommand('cp', ['-R', src_file, suri(dst_bucket_uri, 'dir/foo')])
+    actual = set(
+        str(u) for u in self._test_wildcard_iterator(suri(
+            dst_bucket_uri, '**')).IterAll(expand_top_level_buckets=True))
+    expected = set([suri(dst_bucket_uri, 'dir/foo')])
+    self.assertEqual(expected, actual)
+
+  def testCopyingFileToNonExistentDir(self):
+    """Tests copying a file to a non-existent directory.
+
+    Should create the directory and add the file to it
+    """
+    src_file = self.CreateTempFile(file_name='foo')
+    dst_bucket_uri = self.CreateBucket()
+    self.RunCommand('cp', [src_file, suri(dst_bucket_uri, 'dir') + '/'])
     actual = set(
         str(u) for u in self._test_wildcard_iterator(suri(
             dst_bucket_uri, '**')).IterAll(expand_top_level_buckets=True))