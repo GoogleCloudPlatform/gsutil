d68fd990a041b949bf56fa9d5ceddec480396dfb
Thomas MacLean <thomasmaclean@google.com>
2020-05-04 20:39:39 -0400

    Enhancing gsutil User Agent string to match patterns in gcloud (#986)
    * Enhancing gsutil User Agent string to match patterns in gcloud
    
    * fixing linter errors
    
    * Adding user agent test and fixing storage client user agent.
    
    * handling unused variable
    
    * Adding pylint fixes
    
    * yapf fixes
    
    * user agent should omit command if run without a command
    
    * Removing redundant command/
    
    * Adding a global user agent string to gslib
    
    * CloudToCloud suffix now ensures a cloud destination
    
    * CloudToCloud suffix now ensures a cloud destination
    
    * Fixed logical operator
    
    * Add 'mv' as possible CloudToCloud command
    
    * Fixed comment and used suggested regex method
    
    * Moving exceptions up so pylint passes
    
    * Error handling now happens in place; using system util for interactive detection
    
    * Formatting fixes, removing exception helper

---
+++ gslib/utils/user_agent_helper.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Contains helper for appending user agent information."""
+
+import re
+import sys
+import gslib
+from gslib.utils import system_util
+
+
+def GetUserAgent(args, metrics_off=True):
+  """Using the command arguments return a suffix for the UserAgent string.
+
+  Args:
+    args: str[], parsed set of arguments entered in the CLI.
+    metrics_off: boolean, whether the MetricsCollector is disabled.
+
+  Returns:
+    str, A string value that can be appended to an existing UserAgent.
+  """
+  user_agent = ' gsutil/%s' % gslib.VERSION
+  user_agent += ' (%s)' % sys.platform
+  user_agent += ' analytics/%s ' % ('disabled' if metrics_off else 'enabled')
+  user_agent += ' interactive/%s' % system_util.IsRunningInteractively()
+
+  if len(args) > 0:
+    user_agent += ' command/%s' % args[0]
+
+    if args[0] in ['cp', 'mv', 'rsync']:
+      # Any cp, mv or rsync commands that have both a source and destination in
+      # the cloud should be noted as that represents a unique use case that may
+      # be better served by the transfer service.
+      cloud_uri_pattern = re.compile(r'^(gs|s3)\://')
+      cloud_uris = [arg for arg in args if cloud_uri_pattern.search(arg)]
+      cloud_uri_dst = cloud_uri_pattern.search(args[-1])
+
+      if len(cloud_uris) > 1 and cloud_uri_dst:
+        user_agent += '-CloudToCloud'
+
+  if system_util.InvokedViaCloudSdk():
+    user_agent += ' google-cloud-sdk'
+    if system_util.CloudSdkVersion():
+      user_agent += '/%s' % system_util.CloudSdkVersion()
+
+  return user_agent
