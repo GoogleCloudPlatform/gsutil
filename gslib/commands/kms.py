a71d923ce2619c7292e8593c7f14c77d024c8ee0
Peter Ciszewski <13368751+piotrci@users.noreply.github.com>
2020-06-08 14:11:14 -0700

    Fixing ticks

--- gslib/commands/kms.py
+++ gslib/commands/kms.py
@@ -108,8 +108,8 @@ _ENCRYPTION_DESCRIPTION = """
 
 <B>OPTIONS</B>
   -k <key>      Set the default KMS key for my-bucket using the
-                full path to the key `projects/[project-id]/locations/[location]/keyRings/[key-
-                ring]/cryptoKeys/[my-key]`
+                full path to the key ``projects/[project-id]/locations/[location]/keyRings/[key-
+                ring]/cryptoKeys/[my-key]``
                 
   -w            (used with -k key) Display a warning rather than 
                 failing if gsutil is unable to verify that
