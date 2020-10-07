3ad2d043859563697da4466da0a63f2587e66d8a
Dilip Pednekar <dilip.ped@gmail.com>
2020-10-06 21:33:25 -0400

    Bump timeout to 720 secs

--- gslib/tests/testcase/integration_testcase.py
+++ gslib/tests/testcase/integration_testcase.py
@@ -996,7 +996,7 @@ class GsUtilIntegrationTestCase(base.GsUtilTestCase):
       os.killpg(os.getpgid(p.pid), signal.SIGKILL)
 
     if IS_OSX:
-      timeout_secs = 360
+      timeout_secs = 720
     else:
       timeout_secs = 180
 
