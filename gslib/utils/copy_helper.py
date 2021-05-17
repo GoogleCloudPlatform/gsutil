c45e1b3872ca81f3974b9a2dc04d1a9ade9447ed
Dilip Pednekar <dilip.ped@gmail.com>
2021-05-17 09:50:18 -0400

    Fix comment

--- gslib/utils/copy_helper.py
+++ gslib/utils/copy_helper.py
@@ -2969,7 +2969,7 @@ def _DownloadObjectToFile(src_url,
             'typically happens when using gsutil to download from a subdirectory '
             'created by the Cloud Console (https://cloud.google.com/console)' %
             dst_url.object_name)))
-    # The warning above is needed because before this error might get ignored
+    # The warning above is needed because errors might get ignored
     # for parallel processing.
     raise InvalidUrlError('Invalid destination path: %s' % dst_url.object_name)
 
