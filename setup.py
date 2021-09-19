4752831aa53e49227704a78fbae3f9accbcecd90
Anurag Kumar <mailanu98@gmail.com>
2021-09-19 19:21:20 +0530

    Update setup.py
    updated classifiers

--- setup.py
+++ setup.py
@@ -128,6 +128,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: System :: Filesystems',
         'Topic :: Utilities',
     ],
