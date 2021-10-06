dc79ab8d2f4288feb82e0722a7eb85a207f42db8
Thomas MacLean <thomasmaclean@google.com>
2021-10-06 16:16:06 -0400

    Revert "Allow the removal of project convenience groups. (#1350)"
    This reverts commit 32102985ce0a495fab3e7d8ab833cdfe6c7674b7.

--- gslib/tests/test_iam.py
+++ gslib/tests/test_iam.py
@@ -373,15 +373,6 @@ class TestIamHelpers(testcase.GsUtilUnitTestCase):
     self.assertIn(
         bvle(members=['deleted:user:foo@bar.com?query=param,uid=123?uid=456'],
              role='roles/storage.admin2'), bindings)
-    # Project convenience groups should be removeable.
-    (_, bindings) = bstt(False, 'Deleted:projectOwner')
-    self.assertEquals(len(bindings), 1)
-    self.assertIn(bvle(members=['deleted:projectOwner'], role=''), bindings)
-    (_, bindings) = bstt(False, 'DELETED:projectOwner:admin')
-    self.assertEquals(len(bindings), 1)
-    self.assertIn(
-        bvle(members=['deleted:projectOwner'], role='roles/storage.admin'),
-        bindings)
 
   def test_duplicate_roles(self):
     """Tests that duplicate roles are ignored."""
