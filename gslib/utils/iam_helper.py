928cbb42f15edb489c1c3e7bd7c11e0c8d296345
thomasmaclean <thomasmaclean@google.com>
2021-10-07 13:46:31 -0400

    Re-implementing project convenience group removal functionality

--- gslib/utils/iam_helper.py
+++ gslib/utils/iam_helper.py
@@ -215,19 +215,27 @@ def BindingStringToTuple(is_grant, input_str):
   if not input_str.count(':'):
     input_str += ':'
 
-  # Allows user specified PUBLIC_MEMBERS and TYPES to be case insensitive.
+  # Allows user specified PUBLIC_MEMBERS, DISCOURAGED_TYPES, and TYPES to be
+  # case insensitive.
   tokens = input_str.split(":")
   public_members = {s.lower(): s for s in PUBLIC_MEMBERS}
   types = {s.lower(): s for s in TYPES}
+  discouraged_types = {s.lower(): s for s in DISCOURAGED_TYPES}
   possible_public_member_or_type = tokens[0].lower()
   possible_type = '%s:%s' % (tokens[0].lower(), tokens[1].lower())
+
   if possible_public_member_or_type in public_members:
     tokens[0] = public_members[possible_public_member_or_type]
   elif possible_public_member_or_type in types:
     tokens[0] = types[possible_public_member_or_type]
+  elif possible_public_member_or_type in discouraged_types:
+    tokens[0] = discouraged_types[possible_public_member_or_type]
   elif possible_type in types:
     (tokens[0], tokens[1]) = types[possible_type].split(':')
   input_str = ":".join(tokens)
+
+  # We can remove project convenience members, but not add them.
+  removing_discouraged_type = not is_grant and tokens[0] in DISCOURAGED_TYPES
 
   if input_str.count(':') == 1:
     tokens = input_str.split(':')
@@ -237,7 +245,7 @@ def BindingStringToTuple(is_grant, input_str):
                              input_str)
     if tokens[0] in PUBLIC_MEMBERS:
       (member, roles) = tokens
-    elif tokens[0] in TYPES:
+    elif tokens[0] in TYPES or removing_discouraged_type:
       member = ':'.join(tokens)
       roles = DROP_ALL
     else:
@@ -249,6 +257,9 @@ def BindingStringToTuple(is_grant, input_str):
       # case "deleted:user:foo@bar.com?uid=1234"
       member = ':'.join(tokens)
       roles = DROP_ALL
+    elif removing_discouraged_type:
+      (member_type, project_id, roles) = tokens
+      member = '%s:%s' % (member_type, project_id)
     else:
       (member_type, member_id, roles) = tokens
       _check_member_type(member_type, input_str)
