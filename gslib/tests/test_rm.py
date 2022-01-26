38f12691362852e58787fcbac2c5d4af038602cb
Dilip Pednekar <dilip.ped@gmail.com>
2022-01-25 22:45:38 -0500

    Add tests

--- gslib/tests/test_rm.py
+++ gslib/tests/test_rm.py
@@ -21,6 +21,8 @@ from __future__ import unicode_literals
 
 import os
 import re
+import sys
+from unittest import mock
 
 from gslib.exception import NO_URLS_MATCHED_PREFIX
 from gslib.exception import NO_URLS_MATCHED_TARGET
@@ -31,6 +33,7 @@ import gslib.tests.util as util
 from gslib.tests.util import GenerationFromURI as urigen
 from gslib.tests.util import ObjectToURI as suri
 from gslib.tests.util import SetBotoConfigForTest
+from gslib.tests.util import SetEnvironmentForTest
 from gslib.utils.retry_util import Retry
 
 MACOS_WARNING = (
@@ -739,3 +742,43 @@ class TestRm(testcase.GsUtilIntegrationTestCase):
           stderr)
     else:
       self.assertIn('2 files/objects could not be removed.', stderr)
+
+
+class TestRmUnitTests(testcase.GsUtilUnitTestCase):
+  """Unit tests for gsutil rm."""
+
+  def test_shim_translates_flags(self):
+    bucket_uri = self.CreateBucket()
+    with SetBotoConfigForTest([('GSUtil', 'use_gcloud_storage', 'dry_run')]):
+      with SetEnvironmentForTest({
+          'CLOUDSDK_CORE_PASS_CREDENTIALS_TO_GSUTIL': 'True',
+          'CLOUDSDK_ROOT_DIR': 'fake_dir',
+      }):
+        mock_log_handler = self.RunCommand(
+            'rm',
+            ['-r', '-R', '-a', '-f', suri(bucket_uri)],
+            return_log_handler=True)
+        info_lines = '\n'.join(mock_log_handler.messages['info'])
+        self.assertIn(
+            'Gcloud Storage Command: {} alpha storage rm'
+            ' -r -r -a --continue-on-error {}'.format(
+                os.path.join('fake_dir', 'bin', 'gcloud'), suri(bucket_uri)),
+            info_lines)
+
+  @mock.patch.object(sys, 'stdin')
+  def test_shim_translates_stdin_flag(self, mock_stdin):
+    bucket_uri = self.CreateBucket()
+    object_uri = self.CreateObject(bucket_uri, 'foo', 'abcd')
+    mock_stdin.__iter__.return_value = [suri(object_uri)]
+    with SetBotoConfigForTest([('GSUtil', 'use_gcloud_storage', 'dry_run')]):
+      with SetEnvironmentForTest({
+          'CLOUDSDK_CORE_PASS_CREDENTIALS_TO_GSUTIL': 'True',
+          'CLOUDSDK_ROOT_DIR': 'fake_dir',
+      }):
+
+        mock_log_handler = self.RunCommand('rm', ['-I'],
+                                           return_log_handler=True)
+        info_lines = '\n'.join(mock_log_handler.messages['info'])
+        self.assertIn(
+            'Gcloud Storage Command: {} alpha storage rm'
+            ' -I'.format(os.path.join('fake_dir', 'bin', 'gcloud')), info_lines)
