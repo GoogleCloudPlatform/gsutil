bb0e622b9dbc14fad52826db23ce5c625fc99dbc
Ryan Kohler <ryankohler@google.com>
2022-02-01 14:35:54 -0800

    adding to third party libs

--- gsutil.py
+++ gsutil.py
@@ -84,6 +84,7 @@ THIRD_PARTY_LIBS = [
     ('httplib2', submodule_pyvers),
     ('retry-decorator', ''),
     ('six', ''),  # Python 2 / 3 compatibility dependency
+    ('google-auth-library-python', ''),
 ]
 
 # The wrapper script adds all third_party libraries to the Python path, since
