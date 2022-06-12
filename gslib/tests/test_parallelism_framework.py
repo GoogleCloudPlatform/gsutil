494cd7e6769304206e092caf3c794fd43874077e
Hugo van Kemenade <hugovk@users.noreply.github.com>
2022-06-12 23:24:28 +0300

    Replace deprecated threading.currentThread with current_thread

--- gslib/tests/test_parallelism_framework.py
+++ gslib/tests/test_parallelism_framework.py
@@ -98,7 +98,7 @@ def _ReturnOneValue(cls, args, thread_state=None):
 
 
 def _ReturnProcAndThreadId(cls, args, thread_state=None):
-  return (os.getpid(), threading.currentThread().ident)
+  return os.getpid(), threading.current_thread().ident
 
 
 def _SleepThenReturnProcAndThreadId(cls, args, thread_state=None):
