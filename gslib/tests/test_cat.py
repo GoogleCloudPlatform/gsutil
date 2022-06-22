6dd1999af62df65a931020bd2a3553fdbb6ca9ed
Eitan Jeselsohn <ejeselsohn@google.com>
2022-06-22 09:45:53 -0400

    deleted six import

--- gslib/tests/test_cat.py
+++ gslib/tests/test_cat.py
@@ -19,7 +19,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
-import six
 import sys
 
 from gslib.cs_api_map import ApiSelector
