25c5793a4fed7f68b7c9e345477e325ea23dcda5
Eitan Jeselsohn <ejeselsohn@google.com>
2022-06-24 14:15:52 -0400

    Fixed code to pass kokoro tests
    cat_object is a named pipe. Therefore, the size attribute
    could be None. I added a check to make sure it is not None
    before making a comparison with it.

--- gslib/utils/cat_helper.py
+++ gslib/utils/cat_helper.py
@@ -138,7 +138,7 @@ class CatHelper(object):
             cat_object = blr.root_object
             # This if statement ensures nothing is ouputted and no error
             # is thrown if the user enters an out of bounds range for the object.
-            if start_byte >= cat_object.size:
+            if cat_object.size is not None and start_byte >= cat_object.size:
               sys.stdout = old_stdout
               return 0
             storage_url = StorageUrlFromString(blr.url_string)
