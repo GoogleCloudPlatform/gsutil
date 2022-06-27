87a73d199b7fa3150eca3e7b31c2777e78f7703e
Eitan Jeselsohn <ejeselsohn@google.com>
2022-06-27 09:07:40 -0400

    Ensured a named pipe has a size greater than 0
    Before the change, the if statment did not check that a named pipe has a
    size greater than 0, causing it to exit before moving any data and
    causing the write thread to be stuck.
    
    After the change, there is a check that the named pipe has a size
    greater than 0, avoiding the issue described above.

--- gslib/utils/cat_helper.py
+++ gslib/utils/cat_helper.py
@@ -138,8 +138,9 @@ class CatHelper(object):
             cat_object = blr.root_object
             # This if statement ensures nothing is ouputted and no error
             # is thrown if the user enters an out of bounds range for the object.
-            if getattr(cat_object, 'size',
-                       None) is not None and start_byte >= cat_object.size:
+            if getattr(
+                cat_object, 'size', None
+            ) is not None and cat_object.size > 0 and start_byte >= cat_object.size:
               sys.stdout = old_stdout
               return 0
             storage_url = StorageUrlFromString(blr.url_string)
