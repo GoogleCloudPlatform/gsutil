8cbf3362f34becf358f2fcd60dc3f20bf08a9a17
NickGoog <66492516+NickGoog@users.noreply.github.com>
2022-11-03 16:09:00 -0400

    Shim autoclass command (#1618)

--- gslib/commands/pap.py
+++ gslib/commands/pap.py
@@ -28,7 +28,6 @@ from gslib.exception import NO_URLS_MATCHED_TARGET
 from gslib.help_provider import CreateHelpText
 from gslib.third_party.storage_apitools import storage_v1_messages as apitools_messages
 from gslib.utils.constants import NO_MAX
-from gslib.utils.shim_util import GcloudStorageFlag
 from gslib.utils.shim_util import GcloudStorageMap
 
 _SET_SYNOPSIS = """
