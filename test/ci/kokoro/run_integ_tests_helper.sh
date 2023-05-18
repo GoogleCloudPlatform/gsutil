4e78e3fdb9aacf5b9f2769243c0c286441100556
Dilip Pednekar <dilip.ped@gmail.com>
2023-05-17 22:45:24 -0400

    Test cp only

--- test/ci/kokoro/run_integ_tests_helper.sh
+++ test/ci/kokoro/run_integ_tests_helper.sh
@@ -110,8 +110,8 @@ set -e
 # Check that we're using the correct config.
 python "$GSUTIL_ENTRYPOINT" version -l
 # Run integration tests.
-# python "$GSUTIL_ENTRYPOINT" test -p "$PROCS"
-python "$GSUTIL_ENTRYPOINT" test -p "$PROCS" "cp.TestCp.test_gzip_transport_encoded_parallel_upload_resumable"
+python "$GSUTIL_ENTRYPOINT" test -p "$PROCS" cp
+# python "$GSUTIL_ENTRYPOINT" test -p "$PROCS" "cp.TestCp.test_gzip_transport_encoded_parallel_upload_resumable"
 # Run custom endpoint tests.
 # We don't generate a .boto for these tests since there are only a few settings.
 python "$GSUTIL_ENTRYPOINT" \
