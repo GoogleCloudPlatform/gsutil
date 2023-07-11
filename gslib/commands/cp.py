da69c38bebf63028998387fe3f5be2abb53cab3d
starsandskies <nstock@google.com>
2023-07-11 13:53:44 -0700

    Update cp.py

--- gslib/commands/cp.py
+++ gslib/commands/cp.py
@@ -373,7 +373,7 @@ _COPYING_SPECIAL_FILES_TEXT = """
 
 _OPTIONS_TEXT = """
 <B>OPTIONS</B>
-  -a canned_acl  Applies the specific ``canned_acl`` to uploaded objects. See
+  -a predef_acl  Applies the specific predefined ACL to uploaded objects. See
                  "gsutil help acls" for further details.
 
   -A             Copy all source versions from a source bucket or folder.
